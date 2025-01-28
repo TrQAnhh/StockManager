b3d4a92dbbe862ffc8fe6c9a12d7e7367890012e
Trần Quốc Anh <135408372+TrQAnhh@users.noreply.github.com>
2025-01-28 23:16:46 +0700

    dashboard for admin

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const route = require("./routes/client/index.route");
 const database = require("./config/database");
 const app = express();
 require("dotenv").config();
 
 const port = process.env.PORT;
+
+//  Client route
+const clientRoute = require("./routes/client/index.route");
+
+// Admin routes
+const adminRoute = require("./routes/admin/index.route");
 
 // Database
 database.connect();
@@ -15,7 +20,8 @@ app.set("view engine", "pug");
 app.use(express.static("public"));
 
 // Routes
-route(app);
+clientRoute(app);
+adminRoute(app);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
