0855da89e0e01c6bfdfdba5f09ecd8c91183167c
Trần Quốc Anh <135408372+TrQAnhh@users.noreply.github.com>
2025-01-29 03:22:41 +0700

    - Find active/inactive/all products by buttons

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,6 +2,30 @@ const Product = require('../../models/product.model')
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
+    let filterStatus = [
+        {
+            name: "All",
+            status: "",
+            class: "",
+        },{
+            name: "Active",
+            status: "active",
+            class: "",
+        },{
+            name: "Inactive",
+            status: "inactive",
+            class: "",
+        }
+    ]
+
+    if(req.query.status) {
+        const index = filterStatus.findIndex(item => item.status === req.query.status);
+        filterStatus[index].class = "active"
+    } else { // status is empty (for filter all)
+        const index = filterStatus.findIndex(item => item.status === "");
+        filterStatus[index].class = "active"
+    }
+
     let condition = {
         deleted: false,
     };
@@ -14,5 +38,6 @@ module.exports.index = async (req, res) => {
 
     res.render("admin/pages/product/index",{
         products: products,
+        filterStatus: filterStatus,
     });
 }
