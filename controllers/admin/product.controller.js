259b1b507fb2cec78c8f4484a7ec482786d7869a
Trần Quốc Anh <135408372+TrQAnhh@users.noreply.github.com>
2025-02-05 22:18:56 +0700

    Delete and restore products

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -6,113 +6,52 @@ const paginationHelpers = require('../../helpers/pagination');
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
+    // Initialize the filter condition
+    let condition = {};
 
-    // All / Active / Inactive filters
-        // let filterStatus = [
-        //     {
-        //         name: "All",
-        //         status: "",
-        //         class: "",
-        //     },{
-        //         name: "Active",
-        //         status: "active",
-        //         class: "",
-        //     },{
-        //         name: "Inactive",
-        //         status: "inactive",
-        //         class: "",
-        //     }
-        // ]
-        //
-        // if(req.query.status) {
-        //     const index = filterStatus.findIndex(item => item.status === req.query.status);
-        //     filterStatus[index].class = "active"
-        // } else { // status is empty (for filter all)
-        //     const index = filterStatus.findIndex(item => item.status === "");
-        //     filterStatus[index].class = "active"
-        // }
+    // Filter by deletion status
+    if (req.query.deleted === 'true') {
+        condition.deleted = true;
+    } else {
+        condition.deleted = false;
+    }
 
-        const filterStatus = filterStatusHelpers(req.query);
-    // Submission search
-        let condition = {
-            deleted: false,
-        };
+    // Filter by product status if provided
+    if (req.query.status && req.query.deleted !== 'true') {
+        condition.status = req.query.status;
+    }
 
-        if(req.query.status) {
-            condition.status = req.query.status;
-        }
+    // Implement search functionality
+    const objectSearch = searchHelpers(req.query);
+    if (objectSearch.regex) {
+        condition.title = objectSearch.regex;
+    }
 
-        const objectSearch = searchHelpers(req.query);
+    // Pagination setup
+    const countProducts = await Product.countDocuments(condition);
+    let objectPagination = paginationHelpers(
+        {
+            currentPage: 1,
+            limitItems: 4
+        },
+        req.query,
+        countProducts
+    );
 
-        // let keyword = "";
-        //
-        // if(req.query.keyword) {
-        //     keyword = req.query.keyword;
-        //
-        //     // const regex = /keyword/i;
-        //
-        //     const regex= new RegExp(keyword, "i");
-        //
-        //     condition.title = regex;
-        // }
+    // Fetch products based on the condition and pagination
+    const products = await Product.find(condition)
+        .limit(objectPagination.limitItems)
+        .skip(objectPagination.skip);
 
-
-        if(objectSearch.regex) {
-           condition.title = objectSearch.regex;
-        }
-
-    // End submission search
-
-    // Pagination (phân trang)
-
-        // Before being optimized
-
-            //     let objectPagination = {
-            //         currentPage: 1,
-            //         limitItems: 4
-            //     }
-
-            // Get the current page from query (cần convert từ string -> number (màu vàng))
-            // if(req.query.page) {
-            //     objectPagination.currentPage = parseInt(req.query.page);
-            // }
-
-            // objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems;
-
-            // Count the number of documents we have in product model
-            // const countProducts = await Product.countDocuments(condition);
-
-            // The number of pages = total documents / the number of documents in each page
-            // const totalPage = Math.ceil(countProducts / objectPagination.limitItems);
-
-            //objectPagination.totalPages = totalPage;
-
-        // After optimized
-
-            // Count the number of documents we have in product model
-            const countProducts = await Product.countDocuments(condition);
-
-            let objectPagination = paginationHelpers(
-                {
-                    currentPage: 1,
-                    limitItems: 4
-                },
-                req.query,
-                countProducts
-            );
-
-
-    // End pagination
-
-    const products = await Product.find(condition).limit(objectPagination.limitItems).skip(objectPagination.skip);
-
-    res.render("admin/pages/product/index",{
+    // Render the appropriate view
+    res.render("admin/pages/product/index", {
         products: products,
-        filterStatus: filterStatus,
+        filterStatus: filterStatusHelpers(req.query),
         keyword: objectSearch.keyword,
         pagination: objectPagination,
     });
-}
+};
+
 
 // [PATCH] /admin/products/change-status/:status/:id
 module.exports.changeStatus = async (req, res) => {
@@ -131,5 +70,38 @@ module.exports.changeMulti = async (req, res) => {
     const ids = req.body.ids.split(", ");
 
     await Product.updateMany({_id: { $in: ids }}, {status: type} );
+    res.redirect("back");
+}
+
+// [DELETE] /admin/products/delete/:id
+module.exports.deleteProduct = async (req,res) => {
+    const id = req.params.id;
+
+    // Hard delete
+    // await Product.deleteOne({_id: id});
+
+    // Soft Delete (set deleted attribute of record to true)
+    await Product.updateOne(
+        {_id: id},
+        {deleted: true,
+            deletedAt: new Date(),
+            status: "inactive",
+        },
+    );
+
+    res.redirect("back");
+}
+
+// [PATCH] /admin/products/restore/:id
+module.exports.restoreProduct = async (req,res) => {
+    const id = req.params.id;
+
+    await Product.updateOne(
+        {_id: id},
+        {deleted: false,
+                deletedAt: null,
+        },
+    );
+
     res.redirect("back");
 }
--- helpers/filterStatus.js
+++ helpers/filterStatus.js
@@ -4,23 +4,36 @@ module.exports = (query) => {
             name: "All",
             status: "",
             class: "",
+            deleted: false,
         },{
             name: "Active",
             status: "active",
             class: "",
+            deleted: false,
         },{
             name: "Inactive",
             status: "inactive",
             class: "",
+            deleted: false,
+        },{
+            name: "Deleted",
+            status: "",
+            class: "",
+            deleted: true,
         }
     ]
 
-    if(query.status) {
+
+
+    if(query.status && query.deleted != true) {
         const index = filterStatus.findIndex(item => item.status === query.status);
-        filterStatus[index].class = "active"
-    } else { // status is empty (for filter all)
+        filterStatus[index].class = "active";
+    } else if (query.deleted){ // query.deleted is true
+        const index = filterStatus.findIndex(item => item.status === "" && item.deleted);
+        filterStatus[index].class = "active";
+    } else { // query.status is empty (for filter all)
         const index = filterStatus.findIndex(item => item.status === "");
-        filterStatus[index].class = "active"
+        filterStatus[index].class = "active";
     }
 
     return filterStatus;
